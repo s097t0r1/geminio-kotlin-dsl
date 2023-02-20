ee73e73c0707dff5b30d7f8e630566c6f642660b
s097t0r1 <zmitr241@yandex.ru>
2023-02-20 20:25:26 +0300

    fix: fix enable module creation params from map to list

--- src/main/kotlin/me/s097t0r1/geminio_kotlin_dsl/recipe/predefined_features/PredefinedFeatures.kt
+++ src/main/kotlin/me/s097t0r1/geminio_kotlin_dsl/recipe/predefined_features/PredefinedFeatures.kt
@@ -4,15 +4,11 @@ import me.s097t0r1.geminio_kotlin_dsl.recipe.RecipeNode
 
 class PredefinedFeatures() : RecipeNode {
 
-    private val nodes = mutableMapOf<String, RecipeNode>()
+    private val nodes = mutableListOf<RecipeNode>()
 
     fun enableModuleCreationParams(builder: EnableModuleCreationParams.() -> Unit) {
-        nodes[ENABLE_MODULE_CREATION_PARAMS_KEY] = EnableModuleCreationParams().apply(builder)
+        nodes += EnableModuleCreationParams().apply(builder)
     }
 
-    override fun unwrap(): Any = nodes.mapValues { it.value.unwrap() }
-
-    companion object {
-        private const val ENABLE_MODULE_CREATION_PARAMS_KEY = "enableModuleCreationParams"
-    }
+    override fun unwrap(): Any = nodes.map(RecipeNode::unwrap)
 }
