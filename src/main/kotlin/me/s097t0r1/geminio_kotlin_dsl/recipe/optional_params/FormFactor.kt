efa2def652dd30aaf8c129a41af527679e5652b0
Nikita Zmitrovich <47451179+s097t0r1@users.noreply.github.com>
2023-02-27 15:28:06 +0300

    fix(optional_params): fix incorrect enum values

--- src/main/kotlin/me/s097t0r1/geminio_kotlin_dsl/recipe/optional_params/FormFactor.kt
+++ src/main/kotlin/me/s097t0r1/geminio_kotlin_dsl/recipe/optional_params/FormFactor.kt
@@ -2,8 +2,8 @@ package me.s097t0r1.geminio_kotlin_dsl.recipe.optional_params
 
 enum class FormFactor(val value: String) {
     MOBILE("mobile"),
-    WEAR("mobile"),
-    TV("TV"),
-    AUTOMATIVE(""),
-    GENERIC("")
+    WEAR("wear"),
+    TV("tv"),
+    AUTOMOTIVE("automotive"),
+    GENERIC("generic")
 }
